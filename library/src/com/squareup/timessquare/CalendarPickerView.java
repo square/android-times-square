33657d31b93e4bd57bb7f00cbce89640787ac5aa
Eric Denman <edenman@squareup.com>
2013-05-24 11:30:41 -0700

    More docs on the new RANGE behavior

--- library/src/com/squareup/timessquare/CalendarPickerView.java
+++ library/src/com/squareup/timessquare/CalendarPickerView.java
@@ -38,11 +38,23 @@ import static java.util.Calendar.YEAR;
  */
 public class CalendarPickerView extends ListView {
   public enum SelectionMode {
-    /** Only one date will be selectable. */
+    /**
+     * Only one date will be selectable.  If there is already a selected date and you select a new
+     * one, the old date will be unselected.
+     */
     SINGLE,
-    /** Multiple dates will be selectable. */
+    /**
+     * Multiple dates will be selectable.  If there is already a selected date and you select a new
+     * one, the old date will also be selected.
+     */
     MULTIPLE,
-    /** Allows you to select a date range. */
+    /**
+     * Allows you to select a date range.  Previous selections are cleared when you either:
+     * <ul>
+     * <li>Have a range selected and select another date (even if it's in the current range).</li>
+     * <li>Have one date selected and then select an earlier date.</li>
+     * </ul>
+     */
     RANGE
   }
 
