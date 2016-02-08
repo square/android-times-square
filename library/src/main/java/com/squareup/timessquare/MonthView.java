614f34e0cb6162f0d3c75af9bdfeed5311f38fdd
Eric Denman <edenman@gmail.com>
2016-02-08 12:01:46 -0600

    Add support for Arabic numbers (finishing work by @JulianRayner)

--- library/src/main/java/com/squareup/timessquare/MonthView.java
+++ library/src/main/java/com/squareup/timessquare/MonthView.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import java.text.DateFormat;
+import java.text.NumberFormat;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -19,6 +20,7 @@ public class MonthView extends LinearLayout {
   private Listener listener;
   private List<CalendarCellDecorator> decorators;
   private boolean isRtl;
+  private Locale locale;
 
   public static MonthView create(ViewGroup parent, LayoutInflater inflater,
       DateFormat weekdayNameFormat, Listener listener, Calendar today, int dividerColor,
@@ -49,6 +51,7 @@ public class MonthView extends LinearLayout {
     final int originalDayOfWeek = today.get(Calendar.DAY_OF_WEEK);
 
     view.isRtl = isRtl(locale);
+    view.locale = locale;
     int firstDayOfWeek = today.getFirstDayOfWeek();
     final CalendarRowView headerRow = (CalendarRowView) view.grid.getChildAt(0);
     for (int offset = 0; offset < 7; offset++) {
@@ -100,6 +103,7 @@ public class MonthView extends LinearLayout {
     Logr.d("Initializing MonthView (%d) for %s", System.identityHashCode(this), month);
     long start = System.currentTimeMillis();
     title.setText(month.getLabel());
+    NumberFormat numberFormatter = NumberFormat.getInstance(locale);
 
     final int numRows = cells.size();
     grid.setNumRows(numRows);
@@ -113,7 +117,7 @@ public class MonthView extends LinearLayout {
           MonthCellDescriptor cell = week.get(isRtl ? 6 - c : c);
           CalendarCellView cellView = (CalendarCellView) weekRow.getChildAt(c);
 
-          String cellDate = Integer.toString(cell.getValue());
+          String cellDate = numberFormatter.format(cell.getValue());
           if (!cellView.getDayOfMonthTextView().getText().equals(cellDate)) {
             cellView.getDayOfMonthTextView().setText(cellDate);
           }
