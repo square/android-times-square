c936ee2f7bd981501a02f0e66705cb1e258423dd
Jorge Garrido <firezenk@gmail.com>
2017-02-13 09:43:36 +0100

    Remove unused import to fix checkstyle

--- library/src/main/java/com/squareup/timessquare/CalendarPickerView.java
+++ library/src/main/java/com/squareup/timessquare/CalendarPickerView.java
@@ -23,7 +23,6 @@ import java.util.Calendar;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.TimeZone;
