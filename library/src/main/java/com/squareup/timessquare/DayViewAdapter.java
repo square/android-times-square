66badda98499b8243abba8a71df6e4fe9b162715
James Lendrem <James@kinocreative.co.uk>
2017-10-11 18:21:39 +0100

    Revert "Add Java Doc to full document the makeCellView function."
    This reverts commit b752c994be78f84dfc7da2397d48db15c99691f3.

--- library/src/main/java/com/squareup/timessquare/DayViewAdapter.java
+++ library/src/main/java/com/squareup/timessquare/DayViewAdapter.java
@@ -2,15 +2,5 @@ package com.squareup.timessquare;
 
 /** Adapter used to provide a layout for {@link CalendarCellView}.*/
 public interface DayViewAdapter {
-
-  /**
-   * Make Cell View method is overriden to create a cell view and add it to the parent so it can
-   * be displayed at runtime. This view MUST extend from the Android Text View class to be assured
-   * of working correctly.
-   *
-   * @param parent  The {@link CalendarCellView} Calendar Cell View that holds the current view that
-   *                is being created, the new view needs to be added to this parent in order to be
-   *                displayed correctly.
-   */
   void makeCellView(CalendarCellView parent);
 }
