df2a22c8d1e3200051b18f98733a3dcc1054ca3e
Eric Denman <edenman@users.noreply.github.com>
2018-04-26 15:40:07 -0400

    Allow taller custom cells (#438)

--- library/src/main/java/com/squareup/timessquare/DefaultDayViewAdapter.java
+++ library/src/main/java/com/squareup/timessquare/DefaultDayViewAdapter.java
@@ -1,15 +1,21 @@
 package com.squareup.timessquare;
 
 import android.view.ContextThemeWrapper;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import static android.view.Gravity.CENTER_VERTICAL;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 public class DefaultDayViewAdapter implements DayViewAdapter {
-  @Override
-  public void makeCellView(CalendarCellView parent) {
-      TextView textView = new TextView(
-              new ContextThemeWrapper(parent.getContext(), R.style.CalendarCell_CalendarDate));
-      textView.setDuplicateParentStateEnabled(true);
-      parent.addView(textView);
-      parent.setDayOfMonthTextView(textView);
+  @Override public void makeCellView(CalendarCellView parent) {
+    TextView textView = new TextView(
+        new ContextThemeWrapper(parent.getContext(), R.style.CalendarCell_CalendarDate));
+    textView.setDuplicateParentStateEnabled(true);
+
+    parent.addView(textView,
+        new FrameLayout.LayoutParams(MATCH_PARENT, WRAP_CONTENT, CENTER_VERTICAL));
+    parent.setDayOfMonthTextView(textView);
   }
 }
