eb2724895c357d4df90690d551403832bd2e68c3
androidshahul <android.shahul@gmail.com>
2018-09-26 17:38:02 +0800

    update cell view

--- sample/src/main/java/com/squareup/timessquare/sample/SampleDayViewAdapter.java
+++ sample/src/main/java/com/squareup/timessquare/sample/SampleDayViewAdapter.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.TextView;
 import com.squareup.timessquare.CalendarCellView;
 import com.squareup.timessquare.DayViewAdapter;
+import com.squareup.timessquare.MonthCellDescriptor;
 
 public class SampleDayViewAdapter implements DayViewAdapter {
   @Override public void makeCellView(CalendarCellView parent) {
@@ -12,5 +13,16 @@ public class SampleDayViewAdapter implements DayViewAdapter {
     View layout = inflater.inflate(R.layout.day_view_custom, parent, false);
     parent.addView(layout);
     parent.setDayOfMonthTextView((TextView) layout.findViewById(R.id.day_view));
+  }
+  
+  @Override
+  public void updateCellView(CalendarCellView parent, MonthCellDescriptor cellDescriptor) {
+    if (parent.findViewById(R.id.hour_view) != null) {
+      TextView hoursView = parent.findViewById(R.id.hour_view);
+      if(cellDescriptor != null && cellDescriptor.getDate() != null) {
+        //Showing hours with Random number generator
+        hoursView.setText(""+new Random().nextInt(24));
+      }
+    }
   }
 }
